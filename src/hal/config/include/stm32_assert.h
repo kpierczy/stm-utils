e74019742623665c1eab3b1133ec15c2d6eb661e
Krzysiek Pierczyk <kpierczyk@emka-project.com.pl>
2022-08-19 09:35:13 +0200

    [hal-ll] LL assertion header file

---
+++ src/hal/config/include/stm32_assert.h
@@ -0,0 +1,22 @@
+/* ============================================================================================================================= *//**
+ * @file       stm32f2xx_hal_conf_template.h
+ * @author     Krzysztof Pierczyk (krzysztof.pierczyk@gmail.com)
+ * @maintainer Krzysztof Pierczyk (krzysztof.pierczyk@gmail.com)
+ * @date       Friday, 16th July 2021 11:26:29 am
+ * @modified   Friday, 16th July 2021 2:17:26 pm
+ * @project    stm-utils
+ * @brief      LL assert configuration header for STM32 devices
+ *    
+ * @copyright Krzysztof Pierczyk © 2022
+ * /// ============================================================================================================================ */
+
+#ifndef __STM_UTILS_HAL_CONFIG_STM32_ASSERT_H__
+#define __STM_UTILS_HAL_CONFIG_STM32_ASSERT_H__
+
+/* =========================================================== Includes =========================================================== */
+
+#include "ll_assert.h"
+
+/* ================================================================================================================================ */
+
+#endif
